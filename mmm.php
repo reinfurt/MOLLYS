b1fd5063c9641a0693c3b074043e9f507bbb6382
David Reinfurt <reinfurt@o-r-g.com>
2015-06-24 23:49:55 -0400

    animateX.js first working version

---
+++ mmm.php
@@ -0,0 +1,19 @@
+<html>
+        <head>
+                <title>mmm</title>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="stylesheet" href="GLOBAL/global.css">
+                <script type="text/javascript" src="JS/animateX.js"></script>
+        </head>
+        <body style="background-color:#FFF;">
+		<div>                                
+			<canvas id="canvas1" width="300" height="100"></canvas>
+		</div>
+
+		<script>
+			initX('canvas1','true','100');
+		</script>
+	</body>
+</html>
+
