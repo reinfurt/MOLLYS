b3132bf965009b2ede064c6ec40080ebd9c49f4e
David Reinfurt <reinfurt@o-r-g.com>
2015-08-03 15:31:30 -0400

    adjust timing mechanism in animateX.js (in process)

--- JS/animateX.js
+++ JS/animateX.js
@@ -15,15 +15,202 @@ var gridcolor = "#000";
 var linecolor = "#000";
 var linewidth = 7;
 var pad = linewidth;
-
 var messages = new Array();
 var timeout;
 var pointer;
-
 var canvas;
 var live;
 var context;
 var delay = 200;
+
+
+function initX(canvasId) {
+
+	loadMessages();
+
+	canvas = document.getElementById(canvasId);
+	live = {
+		width: canvas.width, 
+		height: canvas.height
+	};
+	
+	u = live.height;
+	
+	canvas.width = live.width+2*pad;
+	canvas.height = live.height+2*pad;
+	canvas.style.width = (canvas.width/2).toString().concat("px");
+	canvas.style.height = (canvas.height/2).toString().concat("px");
+	
+	context = canvas.getContext("2d");
+	context.strokeStyle = linecolor;
+	context.fillStyle = bgcolor;
+	context.lineWidth = linewidth;
+	
+	keys = Object.keys(messages);
+	k = keys[Math.floor(keys.length*Math.random())];
+	animate(k);
+}
+
+
+function animateX(message) {
+
+	if(pointer < message.length) {
+ 
+                whichcolor = Math.floor(3*Math.random());
+
+		switch (whichcolor) {
+ 
+			case 0:
+			linecolor="#FF0000";
+			break;
+
+			case 1:
+			linecolor="#0000FF";
+			break;
+
+			case 2:
+			linecolor="#FFFF00";
+			break;
+ 
+			default:
+			linecolor="#000000";
+			break;
+		}
+
+		// draw lines
+		var points = message[pointer].split(",");
+
+		context.translate(pad,pad);
+		context.strokeStyle = linecolor;
+		context.beginPath();
+		context.moveTo(points[0]*u, points[1]*u);
+		context.lineTo(points[2]*u, points[3]*u);
+		context.stroke();
+		context.setTransform(1, 0, 0, 1, 0, 0);
+
+		pointer++;
+		timeout = setTimeout(function(){animateX(message);}, delay);
+	} 
+	else {
+
+		stopAnimateX();
+		isDrawing = false;
+                keys = Object.keys(messages);
+                k = keys[Math.floor(keys.length*Math.random())];
+                animate(k);
+	}
+}
+	
+
+function stopAnimateX() {
+
+	if (timeout == null)
+		return true;
+	else {
+
+		clearTimeout(timeout);
+		timeout = null;
+		return false;
+	}
+}
+
+
+// this "helper" is redundant, should remove and use setTimeout only
+// can put all in the stopAnimateX function?
+// make a new function for pausing, then another for starting again?
+
+var isDrawing = false;
+
+function animate(kc) {
+
+	if(!isDrawing) {
+
+		// isDrawing = true;
+		pointer = 0;
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		// drawGrid(canvas, live, context);
+		animateX(messages[kc]);
+	}
+}
+
+	
+var loadv;
+var isLoading = false;
+
+function startLoad() {
+
+	if(!isLoading) {
+
+		loadv = setInterval(load, 100);
+                timeout = setTimeout(function(){animateX(message);}, delay);
+		isLoading = true;
+		console.log("loading");
+	}
+}
+
+
+function load() {
+
+	animate(76);
+}
+
+
+function stopLoad() {
+
+	if(isLoading) {
+
+		clearInterval(loadv);
+		isLoading = false;
+		console.log("done loading");
+	}
+}
+
+
+function drawGrid(canvas,live,context) {
+
+	// draw background
+	context.fillStyle = bgcolor;
+	// context.fillRect(0,0,canvas.width,canvas.height);
+
+	// draw circles
+	// xgrid = 3;
+	// ygrid = 1;
+	radius = linewidth/2;
+	context.fillStyle = gridcolor;
+	context.translate(pad,pad);
+
+	for (x=0; x<=live.width; x+=u) {
+		for (y=0; y<=live.height; y+=u) {
+			// main points
+			context.beginPath();
+			context.arc(x,y,radius,0,2*Math.PI);
+			context.fill();
+			
+			// centre points
+			context.beginPath();
+			context.arc(x+u/2,y+u/2,radius,0,2*Math.PI);
+			context.fill();
+		}
+	}
+	context.setTransform(1, 0, 0, 1, 0, 0);
+}
+
+
+document.onkeydown = function(e) {
+	
+	e = e || window.event;
+	kc = e.which || e.keyCode;
+
+	if (kc in messages)
+		animate(kc);
+	else {
+
+		keys = Object.keys(messages);
+		k = keys[Math.floor(keys.length*Math.random())];
+		animate(k);
+	}
+}
+
 
 function loadMessages() {
 
@@ -195,191 +382,4 @@ function loadMessages() {
 			"3,0,3,1",
 			"3,0.5,2,0.5"
 			];
-}
-
-function initX(canvasId) {
-
-	loadMessages();
-	
-	// get canvas
-	canvas = document.getElementById(canvasId);
-
-	// live = the part being drawn
-	// (canvas is larger than what is drawn b/c padding)
-	// associative arrays
-
-	live = {
-		width: canvas.width, 
-		height: canvas.height
-	};
-	
-	u = live.height;
-	
-	canvas.width = live.width+2*pad;
-	canvas.height = live.height+2*pad;
-	canvas.style.width = (canvas.width/2).toString().concat("px");
-	canvas.style.height = (canvas.height/2).toString().concat("px");
-	
-	context = canvas.getContext("2d");
-	context.strokeStyle = linecolor;
-	context.fillStyle = bgcolor;
-	context.lineWidth = linewidth;
-	
-	keys = Object.keys(messages);
-	k = keys[Math.floor(keys.length*Math.random())];
-	animate(k);
-}
-
-function animateX(message) {
-
-	if(pointer < message.length) {
- 
-		// * todo * add random color picker (r,y,b) per segment
-		// * fix * cleanup
-		// random color
-
-                whichcolor = Math.floor(3*Math.random());
-
-		switch (whichcolor) {
- 
-			case 0:
-			linecolor="#FF0000";
-			break;
-
-			case 1:
-			linecolor="#0000FF";
-			break;
-
-			case 2:
-			linecolor="#FFFF00";
-			break;
- 
-			default:
-			linecolor="#000000";
-			break;
-		}
-
-		// draw lines
-		var points = message[pointer].split(",");
-
-		context.translate(pad,pad);
-		context.strokeStyle = linecolor;
-		context.beginPath();
-		context.moveTo(points[0]*u, points[1]*u);
-		context.lineTo(points[2]*u, points[3]*u);
-		context.stroke();
-		context.setTransform(1, 0, 0, 1, 0, 0);
-
-		pointer++;
-		timeout = setTimeout(function(){animateX(message);}, delay);
-	} 
-	else {
-
-		stopAnimateX();
-		isDrawing = false;
-	}
-}
-	
-function stopAnimateX() {
-
-	if (timeout == null)
-		return true;
-	else {
-
-		clearTimeout(timeout);
-		timeout = null;
-		return false;
-	}
-}
-
-// animateX helper, of sorts
-var isDrawing = false;
-function animate(kc) {
-
-	if(!isDrawing) {
-
-		// isDrawing = true;
-		pointer = 0;
-		context.clearRect(0, 0, canvas.width, canvas.height);
-		drawGrid(canvas, live, context);
-		animateX(messages[kc]);
-	}
-}
-
-var loadv;
-var isLoading = false;
-function startLoad() {
-
-	if(!isLoading) {
-
-		loadv = setInterval(load, 100);
-		isLoading = true;
-		console.log("loading");
-	}
-}
-
-function load() {
-
-	animate(76);
-}
-
-function stopLoad() {
-
-	if(isLoading) {
-
-		clearInterval(loadv);
-		isLoading = false;
-		console.log("done loading");
-	}
-}
-
-function drawGrid(canvas,live,context) {
-
-	// draw background
-	context.fillStyle = bgcolor;
-	// context.fillRect(0,0,canvas.width,canvas.height);
-
-	/*
-	// draw circles
-	//xgrid = 3;
-	//ygrid = 1;
-	radius = linewidth;
-	context.fillStyle = gridcolor;
-	context.translate(pad,pad);
-
-	for (x=0; x<=live.width; x+=u) {
-		for (y=0; y<=live.height; y+=u) {
-			// main points
-			context.beginPath();
-			context.arc(x,y,radius,0,2*Math.PI);
-			context.fill();
-			
-			// centre points
-			context.beginPath();
-			context.arc(x+u/2,y+u/2,radius,0,2*Math.PI);
-			context.fill();
-		}
-	}
-	*/
-	context.setTransform(1, 0, 0, 1, 0, 0);
-}
-
-// helper function 
-
-// * todo * replace this with callback which fires at end of reading array
-// to then and only then listen for another keydown
-
-document.onkeydown = function(e) {
-	
-	e = e || window.event;
-	kc = e.which || e.keyCode;
-
-	if (kc in messages)
-		animate(kc);
-	else {
-
-		keys = Object.keys(messages);
-		k = keys[Math.floor(keys.length*Math.random())];
-		animate(k);
-	}
 }
